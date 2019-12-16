feb6605ce92cb465da936db217fa825f4373f841
Huguy <37883361+Huguy@users.noreply.github.com>
2019-12-16 16:14:59 +0100

    Update BaseEntity.java

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -44,25 +44,5 @@ public class BaseEntity {
         return this.id == null;
     }
     
-    public void func1() {
-        let arr = ["a", "b", "c"];
-
-        let expectedValue = "b";
-        if (expectedValue in arr) { // Noncompliant, will be always false
-            return expectedValue + " found in the array";
-        } else {
-            return expectedValue + " not found";
-        }
-    }
-
-    public void func2() {
-        let arr = ["a", "b", "c"];
-
-        let expectedValue = "1"; // index #1 is corresponding to the value "b"
-        if (expectedValue in arr) { // Noncompliant, will be always true because the array is made of 3 elements and the #1 is always there whatever its value
-            return expectedValue + " found in the array";
-        } else {
-            return expectedValue + " not found";
-        }
-    }
+   
 }
